cb74dc00040e248c51488c3023f1bfff0942865a
isaias-chavez <166086851+isaias-chavez@users.noreply.github.com>
2024-09-06 20:55:14 -0600

    Update image.service.ts

--- src/services/pet/image.service.ts
+++ src/services/pet/image.service.ts
@@ -1,4 +1,6 @@
-
+import { DataSource } from "typeorm";
+import { Image } from "../../typeorm/entities/image-entity";
+import { entities } from "../../typeorm/typeorm.entities";
 
 export class TypeORMImageController {
   private dataSource: DataSource;
