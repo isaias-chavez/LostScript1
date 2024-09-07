38b1c96a66124c1bf41b58024952204c8ab8c019
isaias-chavez <166086851+isaias-chavez@users.noreply.github.com>
2024-09-06 20:58:11 -0600

    Update image.service.ts

--- src/services/pet/image.service.ts
+++ src/services/pet/image.service.ts
@@ -5,8 +5,7 @@ import { entities } from "../../typeorm/typeorm.entities";
 export class TypeORMImageController {
   private dataSource: DataSource;
   constructor() {
-    this.dataSource = new DataSource({
-      type: "mysql",
+    
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
